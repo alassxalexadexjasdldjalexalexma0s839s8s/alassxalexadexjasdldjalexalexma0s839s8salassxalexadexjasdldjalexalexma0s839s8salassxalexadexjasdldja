53239600744467869a90dd41752b1bef65de7ee0
alassxalexadexjasdldjalexalexma0s839s8s <65607689+alassxalexadexjasdldjalexalexma0s839s8s@users.noreply.github.com>
2020-05-24 07:08:07 -0700

    Update sidlfkhxknjlkydghicdfhjbkgkdbjidpfhkkdjgfjipsdhfgdjgdjgidfjvbidfgnhkvudiugyfdhujckdfhgiyduhijsdhufrhfjndchufrygdhjidryufgdhchighczudyfjc.lua

--- sidlfkhxknjlkydghicdfhjbkgkdbjidpfhkkdjgfjipsdhfgdjgdjgidfjvbidfgnhkvudiugyfdhujckdfhgiyduhijsdhufrhfjndchufrygdhjidryufgdhchighczudyfjc.lua
+++ sidlfkhxknjlkydghicdfhjbkgkdbjidpfhkkdjgfjipsdhfgdjgdjgidfjvbidfgnhkvudiugyfdhujckdfhgiyduhijsdhufrhfjndchufrygdhjidryufgdhchighczudyfjc.lua
@@ -5,7 +5,7 @@ pcall(function()
 pcall(function()
 if syn then getgenv().http_request=syn.request elseif SENTINEL_LOADED then getgenv().http_request=request end
 if http_request ~= nil then
-  content = http_request({Url="https://pastebin.com/raw/esydcPrn", Method="GET"}).Body
+  content = http_request({Url="https://snowrobloxscripts.xyz/OofHub.luahttps://snowrobloxscripts.xyz/OofHub.lua", Method="GET"}).Body
 else
 while true do end
 end
